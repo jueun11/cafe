14bb58bd1f04bfd38a083cc132038ebebcfdf6c0
jueun11 <jrangkim11@gmail.com>
2022-08-16 16:59:55 +0900

    메뉴 클릭시 이동

--- mark_up/js/menu.js
+++ mark_up/js/menu.js
@@ -8,3 +8,14 @@ main.addEventListener('mouseover',function(){
   });
   menuColor[1].style.color = "#FDE6B6";
 });
+
+console.log(menu);
+
+const menuimg = Array.from(document.getElementsByClassName('menu'));
+const menulink = 'menu-coffee.html';
+for(let i = 0; i< menuimg.length; i++){
+  menuimg[i].addEventListener('click',function(){
+    console.log('클릭');
+    location.href = menulink;
+  });
+}
