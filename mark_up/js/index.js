fa9000e719df8d17fad717525ed1c717a94f153d
jueun11 <jrangkim11@gmail.com>
2022-08-19 15:44:11 +0900

    메뉴아래 보더 추가

--- mark_up/js/index.js
+++ mark_up/js/index.js
@@ -7,4 +7,6 @@ main.addEventListener('mouseover',function(){
     element.style.color = "#fff";
   });
   menuColor[0].style.color = "#FDE6B6";
+  menuColor[0].style.borderBottom = "3px solid";
+  menuColor[0].style.borderImageLice = "1";
 });
